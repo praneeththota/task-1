df33fab151040841b32d091a416eaaad6720d985
praneeth thota <praneetheee240@gmail.com>
2018-12-04 21:45:39 +0530

    initial commit

---
+++ src/userInput/userInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const userInput = (props) => {
+    return (
+        <div>
+            <input type = "text" onChange = {props.changed} value = {props.userName}/>
+        </div>
+    )
+};
+export default userInput;
---
+++ src/userOutput/userOutput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const userOutput = (props) => {
+    return(
+        <div>
+            <p>username: {props.userName}</p>
+            <p>welcome to text</p>
+        </div>
+    )
+};
+export default userOutput;
